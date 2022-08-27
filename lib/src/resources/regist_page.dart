cf53f1d16012207158bbb587d193fee82bb89ac5
MrHungAnh <truonghunganh93@gmail.com>
2022-08-27 23:48:10 +0700

    text child

--- lib/src/resources/regist_page.dart
+++ lib/src/resources/regist_page.dart
@@ -30,7 +30,7 @@ class _RegisterPageState extends State<RegisterPage> {
               Padding(
                 padding: const EdgeInsets.fromLTRB(0, 40, 0, 6),
                 child: Text(
-                  'Welcome Anh hung anh',
+                  'Welcome Anh hung anh1',
                   style: TextStyle(fontSize: 22, color: Color(0xff333333)),
                 ),
               ),
