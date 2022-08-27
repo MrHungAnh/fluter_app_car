13c684eb42670aa928981d89f43acfcf0a110bde
MrHungAnh <truonghunganh93@gmail.com>
2022-08-27 22:04:36 +0700

    hunganh
    hunganh

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_car/src/app.dart';
+
+void main() {
+runApp(MyApp());
+}
---
+++ lib/src/resources/regist_page.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Color(0xff3277D8)),
+        elevation: 0,
+      ),
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+        constraints: BoxConstraints.expand(),
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Image.asset('assets/images/car-red.jpeg'),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 40, 0, 6),
+                child: Text(
+                  'Welcome Hung Anh',
+                  style: TextStyle(fontSize: 22, color: Color(0xff333333)),
+                ),
+              ),
+              Text(
+                'Sigup with iCab in simple steps',
+                style: TextStyle(fontSize: 16, color: Color(0xff606470)),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 80, 0, 20),
+                child: TextField(
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                  decoration: InputDecoration(
+                      labelText: "Name",
+                      prefixIcon: Container(
+                        width: 50,
+                        child: Image.asset('assets/images/ic_name.png'),
+                      ),
+                      border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xffCED0D2), width: 1),
+                          borderRadius: BorderRadius.all(Radius.circular(6)))),
+                ),
+              ),
+              TextField(
+                style: TextStyle(fontSize: 18, color: Colors.black),
+                obscureText: true,
+                decoration: InputDecoration(
+                    labelText: 'Phone Number',
+                    prefixIcon: Container(
+                      width: 50,
+                      child: Image.asset('assets/images/ic_phone.png'),
+                    ),
+                    border: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Color(0xffCED0D2), width: 1),
+                        borderRadius: BorderRadius.all(Radius.circular(6)))),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                child: TextField(
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                  decoration: InputDecoration(
+                      labelText: "Email",
+                      prefixIcon: Container(
+                        width: 50,
+                        child: Image.asset('assets/images/ic_email.png'),
+                      ),
+                      border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xffCED0D2), width: 1),
+                          borderRadius: BorderRadius.all(Radius.circular(6)))),
+                ),
+              ),
+              TextField(
+                style: TextStyle(fontSize: 18, color: Colors.black),
+                obscureText: true,
+                decoration: InputDecoration(
+                    labelText: 'Password',
+                    prefixIcon: Container(
+                      width: 50,
+                      child: Image.asset('assets/images/ic_password.png'),
+                    ),
+                    border: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Color(0xffCED0D2), width: 1),
+                        borderRadius: BorderRadius.all(Radius.circular(6)))),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 30, 0, 40),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 52,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                    color: Color(0xff3277D8),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(6))),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: RichText(
+                  text: TextSpan(
+                      text: 'Alrealy a Users ! ',
+                      style: TextStyle(fontSize: 16, color: Color(0xff606470)),
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: " Login now !",
+                            style: TextStyle(
+                                fontSize: 16, color: Color(0xff3277D8)))
+                      ]),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
